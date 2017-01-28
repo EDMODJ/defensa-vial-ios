4a5ced41bc45990268e0b681630e2bec96856ef8
EDSON MOISES CORTES MOLINA <edson.cortes@mecabotware.com>
2017-01-28 12:24:51 -0600

    Initial Commit

---
+++ DefensaVial/SecondViewController.swift
@@ -0,0 +1,25 @@
+//
+//  SecondViewController.swift
+//  DefensaVial
+//
+//  Created by EDSON MOISES CORTES MOLINA on 1/28/17.
+//  Copyright © 2017 mecabotware. All rights reserved.
+//
+
+import UIKit
+
+class SecondViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
